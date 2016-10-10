41735b4fdb3345f53d80af3f48ef5cff933d8a06
daniel <dgjeyes@gmail.com>
2016-10-10 18:59:29 +0100

    First

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -14,8 +14,23 @@ service "httpd" do
 	action [ :enable, :start ]
 end
 
-node.default["apache"]["indexfile"] = "index2.html"
-cookbook_file "/var/www/html/index.html" do
-	source node["apache"]["indexfile"]
-	mode "0644"
+execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.disabled" do
+	only_if do
+		File.exist?("/etc/httpd/conf.d/welcome.conf")
+	end
+	notifies :restart, "service[httpd]"
 end
+
+node["apache"]["sites"].each do |site_name, site_data|
+	document_root = "/srv/apache/#{site_name}"
+
+template "/etc/httpd/conf.d/#{site_name}.conf" do
+	source "custom.erb"
+	mode "0644"
+	variables(
+		:document_root => document_root,
+		:port => site_data["port"]
+		)
+		notifies :restart, "service[httpd]"
+	end
+	
